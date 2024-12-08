5b8094f7982c2145eb4d77473c606c460328bc37
yaswanth11156 <136198206+yaswanth11156@users.noreply.github.com>
2024-12-08 12:42:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function letterCombinations(input_digit) {
   //Complete the function
+	cont obj = {
+		2:"abc",
+		3:"def",
+		4:"ghi",
+		5:"jkl",
+		6:"mno",
+		7:"pqrs",
+		8:"tuv",
+		9: "wxyz"
+		
+	}
 }
 
 module.exports = letterCombinations;
